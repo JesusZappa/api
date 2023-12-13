34887a4983bf47d75da2ef79f63ff44e67969519
Jesus-zappa <jjddzzaa@gmail.com>
2023-12-12 20:56:54 -0500

    12/12/2023
    12/12/2023

--- API/js/index.js
+++ API/js/index.js
@@ -1,5 +1,39 @@
 const grid = document.querySelector(".grid");
 
-const fetchCharacter = () =>  {
-    
-}
+const fetchCharacters = (quantity) => {
+  fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${quantity}`)
+    .then((response) => response.json())
+    .then(data => {
+      console.log(data);
+      createCharacterCards(data);
+    });
+};
+
+const createCharacterCards = (data) => {
+  let cards = "";
+  data.forEach(item => {
+    // Extract character data
+    const name = item.character;
+    const quote = item.quote;
+    const imageURL = item.image;
+
+    // Generate the HTML card
+    cards += `
+      <div class="character_card">
+        <div class="character_car">
+          <div class="img_container">
+            <img class="character_img" src="${imageURL}" alt="">
+          </div>
+          <div class="info_character">
+            <h4 class="character_name">${item.character}</h4>
+            <small> ${item.character} dice </small>
+            <p>${item.quote}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  grid.innerHTML = cards;
+};
+
+fetchCharacters(10);
