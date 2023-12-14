292e919b2e77f7b768235a004f680cbe23f63eaa
Jesus-zappa <jjddzzaa@gmail.com>
2023-12-13 19:56:39 -0500

    13/12/2023
    13/12/2023

--- API/js/index.js
+++ API/js/index.js
@@ -1,10 +1,39 @@
 const grid = document.querySelector(".grid");
+const searchInput = document.querySelector(".search_input");
+let searchTimeout;
+
+const searchCharacters = (searchText) => {
+  fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=80`)
+    .then((response) => response.json())
+    .then(data => {
+      const filteredData = data.filter(item => {
+        return item.character.toLowerCase().includes(searchText.toLowerCase()) ||
+               item.quote.toLowerCase().includes(searchText.toLowerCase());
+      });
+
+      createCharacterCard(filteredData[0]); // Mostrar solo la primera coincidencia
+    });
+};
+
+const handleSearch = () => {
+  clearTimeout(searchTimeout);
+  const searchText = searchInput.value.trim();
+
+  searchTimeout = setTimeout(() => {
+    if (searchText !== "") {
+      searchCharacters(searchText);
+    } else {
+      fetchCharacters(20);
+    }
+  }, 500);
+};
+
+searchInput.addEventListener("input", handleSearch);
 
 const fetchCharacters = (quantity) => {
   fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${quantity}`)
     .then((response) => response.json())
     .then(data => {
-      console.log(data);
       createCharacterCards(data);
     });
 };
@@ -12,12 +41,10 @@ const fetchCharacters = (quantity) => {
 const createCharacterCards = (data) => {
   let cards = "";
   data.forEach(item => {
-    // Extract character data
     const name = item.character;
     const quote = item.quote;
     const imageURL = item.image;
 
-    // Generate the HTML card
     cards += `
       <div class="character_card">
         <div class="character_car">
@@ -36,6 +63,30 @@ const createCharacterCards = (data) => {
   grid.innerHTML = cards;
 };
 
-fetchCharacters(10);
+const createCharacterCard = (item) => {
+  if (item) {
+    const name = item.character;
+    const quote = item.quote;
+    const imageURL = item.image;
 
+    const card = `
+      <div class="character_card">
+        <div class="character_car">
+          <div class="img_container">
+            <img class="character_img" src="${imageURL}" alt="">
+          </div>
+          <div class="info_character">
+            <h4 class="character_name">${name}</h4>
+            <small> ${name} dice </small>
+            <p>${quote}</p>
+          </div>
+        </div>
+      </div>
+    `;
+    grid.innerHTML = card;
+  } else {
+    grid.innerHTML = "<p>No se encontraron resultados</p>";
+  }
+};
 
+fetchCharacters(20);
